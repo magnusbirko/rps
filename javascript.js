df4e527929681adcc913f81315875326e867b2d8
magnusbirko <magnusbirko@gmail.com>
2024-11-18 20:35:03 +0100

    Game fixed

--- javascript.js
+++ javascript.js
@@ -1,29 +1,86 @@
-let computerScore = 0;
-let humanScore = 0;
+const choices = ["rock", "paper", "scissors"];
+const winners = [];
+
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
+  }
+  logWins();
+}
+
+function playRound(round) {
+  const playerSelection = playerChoice();
+  const computerSelection = computerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
+}
+
+function playerChoice() {
+  let input = prompt("Rock, Paper or Scissors?");
+  while (input === null) {
+    input = prompt("Rock, Paper or Scissors?");
+  }
+  input = input.toLowerCase();
+  let check = validateInput(input);
+
+  while (check === false) {
+    input = prompt("Rock, Paper or Scissors?, please spell correctly!");
+    while (input === null) {
+      input = prompt("Rock, Paper or Scissors?");
+    }
+    input = input.toLowerCase();
+    check = validateInput(input);
+  }
+
+  return input;
+}
 
 function computerChoice() {
-  randNumb = Math.floor(Math.random() * 3);
-  return randNumb;
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function userInput() {
-  let input = prompt("Rock, Paper or Scissor!").toLowerCase();
-  if (input === "rock") {
-    return 0;
-  } else if (input === "paper") {
-    return 1;
-  } else if (input === "scissor") {
-    return 2;
-  } else userInput();
+function validateInput(input) {
+  return choices.includes(input);
 }
 
-function playRound() {
-  if (Number(userInput()) === computerChoice()) {
-    alert("You won a point!");
-    humanScore++;
+function checkWinner(choiceP, choiceC) {
+  if (choiceP === choiceC) {
+    alert("Draw, no points awarded");
+    return "Tie";
+  } else if (
+    (choiceP === "rock" && choiceC === "scissors") ||
+    (choiceP === "paper" && choiceC === "rock") ||
+    (choiceP === "scissors" && choiceC === "paper")
+  ) {
+    alert("You win, 1 point awarded!");
+    return "Player";
   } else {
-    alert("Computer wins a point!");
-    computerScore++;
+    alert("Computer wins, 1 point awarded!");
+    return "Computer";
   }
 }
-playRound();
+
+function logWins() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let ties = winners.filter((item) => item == "Tie").length;
+
+  console.log("Results:");
+  console.log("Player wins:", playerWins);
+  console.log("Computer wins:", computerWins);
+  console.log("Ties:", ties);
+}
+
+function logRound(playerChoice, computerChoice, winner, round) {
+  console.log("Round: ", round);
+  console.log("Player chose:", playerChoice);
+  console.log("Computer chose:", computerChoice);
+  if (winner === "Tie") {
+    console.log("Round was a", winner);
+  } else {
+    console.log("Winner is:", winner);
+  }
+}
+
+game();
